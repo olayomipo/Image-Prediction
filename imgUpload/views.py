bf3c6837f8ddd437d360fb651426c67bbdc2947c
LilyCrown999 <adexadexdede@gmail.com>
2022-08-29 17:52:32 +0100

    display added

--- imgUpload/views.py
+++ imgUpload/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
 
+from email.mime import application
+from django.shortcuts import render
 from .forms import ImageUploadForm
+
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+
+
 
 
 def handle_upload_file(f):
@@ -11,10 +19,23 @@ def handle_upload_file(f):
 def home(request):
     return render(request, 'home.html')
 
+def uploadimage(request):
+    return render(request, 'UploadImage.html')
+
 def imageprocess(request):
     form = ImageUploadForm(request.POST, request.FILES)
     if form.is_valid():
         handle_upload_file(request.FILES['image'])
-        print(request.FILES['image'])
 
-    return render(request, 'result.html')
+        model = ResNet50(weights='imagenet')
+        img_path = 'img.jpg'
+
+        img = image.load_img(img_path, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+
+        preds = model.predict(x)
+        print('Predicted:', decode_predictions(preds, top=3)[0])
+        
+    return render(request, 'UploadImage.html')
