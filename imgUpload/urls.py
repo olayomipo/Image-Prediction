cfe4b42758d07a037c26be54d515a1795b49326c
LilyCrown999 <adexadexdede@gmail.com>
2022-08-29 03:02:43 +0100

    Second commit

--- imgUpload/urls.py
+++ imgUpload/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('imageprocess', views.imageprocess, name='imageprocess')
 ]
--- imgUpload/views.py
+++ imgUpload/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import ImageUploadForm
+
+
+def handle_upload_file(f):
+    with open('img.jpg', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
 def home(request):
     return render(request, 'home.html')
+
+def imageprocess(request):
+    form = ImageUploadForm(request.POST, request.FILES)
+    if form.is_valid():
+        handle_upload_file(request.FILES['image'])
+        print(request.FILES['image'])
+
+    return render(request, 'result.html')
